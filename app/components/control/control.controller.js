561132645cc336bda6fd7ad3e2cc53ecf83f79c6
LeThang <32878676+LeTrongThang@users.noreply.github.com>
2018-04-13 23:39:25 +0700

    13/4

--- app/components/control/control.controller.js
+++ app/components/control/control.controller.js
@@ -34,10 +34,10 @@
         vm.connected2 = false;
         vm.wait2 = false;
         vm.listType = [];
-        var topicPublish1 = "doan";
+        var topicPublish1 = "control1";
         var topicPublish2 = "control2";
         // var client = new Paho.MQTT.Client("host", port, "client_id");
-        var client = new Paho.MQTT.Client("m10.cloudmqtt.com", 	34737, "web_" + parseInt(Math.random() * 100, 10));
+        var client = new Paho.MQTT.Client("m13.cloudmqtt.com", 	37587, "web_" + parseInt(Math.random() * 100, 10));
         var client2 = new Paho.MQTT.Client("m20.cloudmqtt.com", 32666, "web_" + parseInt(Math.random() * 100, 10));
         // set callback handlers
         client.onConnectionLost = onConnectionLost;
@@ -53,8 +53,8 @@
         vm.send = send;
         var options = {
             useSSL: true,
-            userName: "qajdsnfn",
-            password: "ankemOeMn0qZ",
+            userName: "pnhhztcw",
+            password: "4pTHr5CT-jyB",
             onSuccess: onConnect,
             onFailure: doFail
         }
